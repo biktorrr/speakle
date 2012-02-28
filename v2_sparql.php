0ef5a9073e18bbd5604762ecd3e6cb2f520a14e9
Victor de Boer <v.de.boer@vu.nl>
2012-02-28 16:10:40 +0100

    added sparqltester

--- v2_sparql.php
+++ v2_sparql.php
@@ -1,9 +1,17 @@
 <?php
 
-	$product = $_['product'];
+	#$product = $_['product'];
+	$product = "Beurre de karite";
+	$myquery = "SELECT DISTINCT ?p 
+WHERE { 
+?p <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> rm:Person . 
+?o <http://purl.org/collections/w4ra/radiomarche/has_contact> ?p . 
+?o <http://purl.org/collections/w4ra/radiomarche/prod_name> ?pn . 
+?pn <http://www.w3.org/2000/01/rdf-schema#label> '".$product ."'}";
 	
-	#$result = file_get_contents('http://eculture.cs.vu.nl/sparql/?q=' . $product);
-	$result = file_get_contents('http://eculture.cs.vu.nl:1979/sparql/?query=SELECT%20DISTINCT%20?label%20WHERE%20{%20?p%20rdfs:label%20?label}%20LIMIT%2050&format=json');
+	
+	$result = file_get_contents('http://eculture.cs.vu.nl/sparql/?q=' .urlencode($myquery));
+	#$result = file_get_contents('http://eculture.cs.vu.nl:1979/sparql/?query=SELECT%20DISTINCT%20?label%20WHERE%20{%20?p%20rdfs:label%20?label}%20LIMIT%2050&format=json');
 	print $result;
 
 ?>

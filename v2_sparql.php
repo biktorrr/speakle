ea669f69b482c537c7fdf0c462fe8797383eb1e8
Victor de Boer <v.de.boer@vu.nl>
2012-02-28 16:12:09 +0100

    added sparqltester

--- v2_sparql.php
+++ v2_sparql.php
@@ -9,8 +9,10 @@ WHERE {
 ?o <http://purl.org/collections/w4ra/radiomarche/prod_name> ?pn . 
 ?pn <http://www.w3.org/2000/01/rdf-schema#label> '".$product ."'}";
 	
+	$encoded_query = urlencode($myquery);
+	print $encoded_query;
 	
-	$result = file_get_contents('http://eculture.cs.vu.nl/sparql/?q=' .urlencode($myquery));
+	$result = file_get_contents('http://eculture.cs.vu.nl/sparql/?q=' .$encoded_query);
 	#$result = file_get_contents('http://eculture.cs.vu.nl:1979/sparql/?query=SELECT%20DISTINCT%20?label%20WHERE%20{%20?p%20rdfs:label%20?label}%20LIMIT%2050&format=json');
 	print $result;
 
